2ada2d6d38fe55a7a17ea7870a59216ef0d6f7bd
HemanthKumar M <m.hemanthkmr143@gmail.com>
2023-10-17 18:14:03 +0000

    Added a Todo list to the users and generateAurhToken

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -14,7 +16,30 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  todos: [
+    {
+      text: {
+        type: String,
+        required: true,
+      },
+      completed: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  ],
 });
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, name: this.name, email: this.email },
+    "jwtPrivateKey",
+    {
+      expiresIn: 86400, // 24 hours
+    }
+  );
+  return token;
+};
 
 const User = mongoose.model("User", userSchema);
 
